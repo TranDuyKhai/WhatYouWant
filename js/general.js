890a6e8ed75a56f6e0e9ccb3bf655c703361cb3f
Will <khaitran610@gmail.com>
2020-08-10 17:15:33 +0700

    done layout
    will

--- js/general.js
+++ js/general.js
@@ -1,7 +1,5 @@
 $(function(){
-
 	
-
 	$('#backTop').on('click', function(e) {
 		e.preventDefault();
 		$('html, body').animate({scrollTop: 0}, 'slow');
---
+++ js/slideshows.js
@@ -0,0 +1,93 @@
+$(function(){
+
+	$('.mv .blockSlide__slide').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		cssEase: 'linear',
+		autoplay: false,
+		autoplaySpeed: 2000,
+	});
+	$('.category').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		cssEase: 'linear',
+		autoplay: false,
+		autoplaySpeed: 2000,
+	});
+	$('.viewmore').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		cssEase: 'linear',
+		autoplay: false,
+		autoplaySpeed: 2000,
+	});
+	$('.news').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		cssEase: 'linear',
+		autoplay: false,
+		autoplaySpeed: 2000,
+	});
+	$('.events').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		cssEase: 'linear',
+		autoplay: false,
+		autoplaySpeed: 2000,
+	});
+	$('.twoblock').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		cssEase: 'linear',
+		autoplay: false,
+		autoplaySpeed: 2000,
+	});
+	$('.topComment').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		cssEase: 'linear',
+		autoplay: false,
+		autoplaySpeed: 2000,
+	});
+	$('.showing').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		cssEase: 'linear',
+		autoplay: false,
+		autoplaySpeed: 2000,
+	});
+	$('.oscars .blockSlide__slide').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		cssEase: 'linear',
+		autoplay: false,
+		autoplaySpeed: 2000,
+	});
+});
