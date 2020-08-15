89f9e86363d07cdcf3fe3e9b334c1a3d39c4c152
will <khaitran610@gmail.com>
2020-08-15 17:08:35 +0700

    responsive header footer

--- js/general.js
+++ js/general.js
@@ -22,6 +22,15 @@ $(function(){
 			$('.header__navbar__menu').addClass('expand');
 		}
 	});
+	$('.user__noti').on('click', function(){
+		if($('.header__navbar__notification').is('.expand')){
+			$('.header__navbar__notification').removeClass('expand');
+			$('.header__navbar__notification').css({'height': 0,'padding-top': 0});
+		}else{
+			$('.header__navbar__notification').addClass('expand');
+			$('.header__navbar__notification').css('height', $('.header__navbar__notification--noti').outerHeight()*$('.header__navbar__notification--noti').length+'px');
+		}
+	});
 	$('#backTop').on('click', function(e) {
 		e.preventDefault();
 		$('html, body').animate({scrollTop: 0}, 'slow');
