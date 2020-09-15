2bf128785f1fefd34e12abb6ed07448d17b48c02
unknown <nghiaweb@gmail.com>
2020-09-15 17:26:26 +0700

    update js

--- js/main.js
+++ js/main.js
@@ -1,99 +1,27 @@
-$(function(){
+$(".read-more").on("click", function(){
+	// declare text for read more label
+	var text_more;
+	// declare target collapsible content
+	var content_collapse = $(this).data("target");
+	// declare text for close label
+	var text_close = $("[data-collapse="+content_collapse+"]").data("text-close");
 
-	var ctx = document.getElementById('rating-chart').getContext('2d');
-	var data = JSON.parse("[" + $('.rating-chart').data('value') +','+ $('.rating-chart').data('max') + "]");
-
-	var myChart = new Chart(ctx, {
-		type: 'bar',
-		data: {
-			labels: ['1 star', '2 star', '3 star', '4 star', '5 star'],
-			datasets: [{
-				data: data,
-				backgroundColor: ['#00A4D4','#00A4D4','#00A4D4','#00A4D4','#00A4D4','#00A4D4',],
-			}]
-		},
-		options: {
-			legend: {
-				display: false,
-			},
-			tooltips: {
-				enabled: false,
-			},
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero: true
-					}
-				}],
-				xAxes: [{
-					ticks: {
-						display: false
-					}
-				}]
-			}
-		},
-	});
-
-	var doughnut = document.getElementById('country-chart').getContext('2d');
-	var data_doughnut = JSON.parse("[" + $('.country-chart').data('value') + "]"),
-		labels_doughnut = $('.country-chart').data('value').split(',');
-	var myChart = new Chart(doughnut, {
-		type: 'doughnut',
-		data: {
-			labels: ['Anh-Mỹ', 'Trung Quốc', 'Pháp', 'Nhật', 'Thái Lan'],
-			datasets: [{
-				data: data_doughnut,
-				backgroundColor: ['#FF5252','#FFCA52','#00A4D4','#8952FF','#C4C4C4'],
-				borderColor:['#FF5252','#FFCA52','#00A4D4','#8952FF','#C4C4C4'],
-			}]
-		},
-		options: {
-			legend: {
-				display: false,
-			},
-			cutoutPercentage: 25,
-			tooltips: {
-				enabled: false,
-			},
-		},
-	});
-
-	var distribution = document.getElementById('distribution-chart').getContext('2d');
-	var data_distribution = JSON.parse("[" + $('.distribution-chart').data('value') +','+ $('.distribution-chart').data('max') + "]"),
-		data_label = $('.distribution-chart').data('label').split(',');
+	// if content is not collapsed
+	if(!$(this).hasClass('collapse-open')){
+		$(this).data("text-more", $(this).html());
+		text_more = $(this).data("text-more");
+		$(this).addClass('collapse-open');
+		$(this).html(text_close);
+		$("[data-collapse="+content_collapse+"]").slideDown(300);
 
 
-	var distribution_chart = new Chart(distribution, {
-		type: 'line',
-		data: {
-			labels: data_label,
-			datasets: [{
-				label: "Unavailable",
-				fill: true,
-				backgroundColor: '#00A4D4',
-				pointBackgroundColor: '#FFFFFF',
-				borderColor: '#00A4D4',
-				pointHighlightStroke: '#FFFFFF',
-				borderCapStyle: 'butt',
-				data: data_distribution,
+	} else {
+		// if content is collapsed
+		text_more = $(this).data("text-more");
+		$(this).html(text_more);
+		$(this).removeClass('collapse-open');
+		$("[data-collapse="+content_collapse+"]").slideUp(300);
+	}
 
-			}]
-		},
-		options: {
-			responsive: false,
-			scales: {
-				yAxes: [{
-					stacked: true,
-				}],
-				xAxes: [{
-					ticks: {
-						display: false
-					}
-				}]
-			},
-			animation: {
-				duration: 750,
-			},
-		}
-	});
-});
+
+})
